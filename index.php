b09752409eaa03d94a70b89e171a710266979451
devatha k <k.n.devs@gmail.com>
2016-06-28 19:42:08 -0400

    don't give up commit

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<h3> Php here </h3> 
+<html>
+
+ <head>
+  <title>PHP Test</title>
+ </head>
+
+ <body>
+
+
+<?php echo '<p>Hello World</p>'; ?> 
+
+<?/* php 
+  phpinfo()
+*/?>
+
+<?php
+// here we are using a predefined variable. see phpinfo() for more info
+echo $_SERVER['HTTP_USER_AGENT'];
+?>
+
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+    echo 'You are using Internet Explorer.<br />';
+}
+?>
+
+ </body>
+
+</html>
