55c517abfb584935cadc24d8191d57da6deb327b
devatha k <k.n.devs@gmail.com>
2016-06-28 20:04:49 -0400

    if else and forms

--- index.php
+++ index.php
@@ -19,10 +19,39 @@ echo $_SERVER['HTTP_USER_AGENT'];
 ?>
 
 <?php
+// strpos is a predefined function that takes in two arguments
 if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
     echo 'You are using Internet Explorer.<br />';
 }
 ?>
+
+
+<?php 
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+?>
+you are using Internet explorer 
+<?php
+} else {
+?>
+you are not using Internet Explorer
+<?php
+}
+?> 
+
+<!-- this will return not found on server-->
+<form action ="jupiter.php" method="post">
+	<p>Planet name: <input name="planetname" /></p>
+	<input type="submit" value="Submit">
+</form>
+
+
+<!-- this is cool. I like it-->
+<form action ="mars.php" method="post">
+	<p>Name: <input name="personname" /></p>
+	<p>Age: <input name="personage" /></p>
+	<input type="submit" value="Submit">
+</form>
+
 
  </body>
 
---
+++ mars.php
@@ -0,0 +1,2 @@
+<?php echo htmlspecialchars($_POST['personname']); ?>.
+ <?php echo (int)$_POST['personage']; ?> years old.
